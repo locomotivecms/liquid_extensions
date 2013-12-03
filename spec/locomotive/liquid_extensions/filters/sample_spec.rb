0f4e8316cddababf0ef566622a459323587a2937
Ivan Yurov <ivan.youroff@gmail.com>
2013-12-03 15:39:06 +0400

    Sample returns single item when asked for 1 element, or called sample without parameters

--- spec/locomotive/liquid_extensions/filters/sample_spec.rb
+++ spec/locomotive/liquid_extensions/filters/sample_spec.rb
@@ -4,10 +4,19 @@ describe Locomotive::LiquidExtensions::Filters::Sample do
 
   include Locomotive::LiquidExtensions::Filters::Sample
 
-  it 'return a sample of an array' do
+  it 'returns random element of an array' do
     array = ["Foo", "Bar", "Locomotive"]
-    (array - sample(array, 1)).size.should eql 2
+    sample(array, 1).should be_a String
+    array.should include(sample(array, 1))
   end
-
+  
+  it 'return the number of random elements of an array' do
+    array = ["Foo", "Bar", "Locomotive"]
+    smpl = sample(array, 2) 
+    smpl.size.should eql 2
+    smpl.first.should_not eql smpl.last
+    array.should include(smpl.first)
+    array.should include(smpl.last)
+  end
 
 end
