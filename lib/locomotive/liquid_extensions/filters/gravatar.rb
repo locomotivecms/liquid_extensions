a86f88bdb058e59e8a8503215b4af3577bc4af4e
Michael Miller <bluepixelmike@gmail.com>
2015-03-18 06:15:00 +0000

    Added optional arguments for size, default, and rating.

--- lib/locomotive/liquid_extensions/filters/gravatar.rb
+++ lib/locomotive/liquid_extensions/filters/gravatar.rb
@@ -5,14 +5,32 @@ module Locomotive
     module Filters
       module Gravatar
 
-        def gravatar_url(input)
+        def gravatar_url(input, *opts)
           cleaned = input.strip.downcase
           hash    = Digest::MD5.hexdigest(cleaned)
-          "http://www.gravatar.com/avatar/#{hash}"
+          url     = "http://www.gravatar.com/avatar/#{hash}"
+
+          params = {}
+          opts.each do |opt|
+            if m = /^s(ize)?:(\d+)$/i.match(opt)
+              params[:s] = m[2]
+            elsif m = /^d(default)?:(\w+)$/i.match(opt)
+              params[:d] = m[2].downcase
+            elsif m = /^r(ating)?:(g|pg|r|x)$/i.match(opt)
+              params[:r] = m[2].downcase
+            end
+          end
+
+          if params.empty?
+            url
+          else
+            param_str = params.map{|k,v| "#{k}=#{v}"}.join('&')
+            "#{url}?#{param_str}"
+          end
         end
 
-        def gravatar_tag(input)
-          url = gravatar_url(input)
+        def gravatar_tag(input, *opts)
+          url = gravatar_url(input, *opts)
           "<img src=\"#{url}\" alt=\"Gravatar\" class=\"gravatar\" />"
         end
 
