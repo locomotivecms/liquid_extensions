f5aeda446f92c78ab0a71993cb22e7de25f4ac96
Michael Miller <bluepixelmike@gmail.com>
2015-03-18 05:35:20 +0000

    Added spec for gravatar filters.

---
+++ spec/locomotive/liquid_extensions/filters/gravatar_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Locomotive::LiquidExtensions::Filters::Gravatar do
+
+  include Locomotive::LiquidExtensions::Filters::Gravatar
+
+  it 'generates a correct url' do
+    email    = 'me@example.com'
+    expected = 'http://www.gravatar.com/avatar/2e0d5407ce8609047b8255c50405d7b1'
+    actual   = gravatar_url(email)
+    actual.should be_a String
+    actual.should eql expected
+  end
+
+  it 'generates a proper img tag' do
+    email = 'me@example.com'
+    tag   = gravatar_tag(email)
+    tag.should be_a String
+    tag.should start_with '<img '
+    tag.should end_with '/>'
+    tag.should include 'src="http://www.gravatar.com/avatar/2e0d5407ce8609047b8255c50405d7b1"'
+  end
+
+end
